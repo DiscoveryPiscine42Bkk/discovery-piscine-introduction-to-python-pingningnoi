168a9354a192af7f193ba86422602e107a2ddb47
pingningnoi <ping.ningnoi@gmail.com>
2025-11-06 12:55:43 +0000

    completed

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,12 @@
+def add_one(x):
+
+    x = x + 1
+    print(f"Inside add_one: x is now {x}")
+
+my_number = 10
+
+print(f"Before calling add_one: my_number is {my_number}")
+
+add_one(my_number)
+
+print(f"After calling add_one: my_number is {my_number}")
