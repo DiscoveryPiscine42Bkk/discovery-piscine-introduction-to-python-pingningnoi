c82cbdb155c2c8a758accef71811b42fd46cad7c
pingningnoi <ping.ningnoi@gmail.com>
2025-11-06 03:43:41 +0000

    name.py completed

---
+++ cell01/ex00/name.py
@@ -0,0 +1,5 @@
+[span_0](start_span)[span_1](start_span)first_name = "Chalakorn"[span_0](end_span)[span_1](end_span)
+
+[span_2](start_span)[span_3](start_span)last_name = "Cherdchusuwan"[span_2](end_span)[span_3](end_span)
+
+[span_4](start_span)[span_5](start_span)print(first_name, last_name)[span_4](end_span)[span_5](end_span)
