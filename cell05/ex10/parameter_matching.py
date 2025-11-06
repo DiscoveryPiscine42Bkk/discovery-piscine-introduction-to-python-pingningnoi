e9cf3e8b0258bfbb3aa885ed2024c43fb5e52abf
pingningnoi <ping.ningnoi@gmail.com>
2025-11-06 08:46:25 +0000

    completed

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("none")
+
+else:
+    expected_parameter = sys.argv[1]
+  
+    user_input = input("What was the parameter? ")
+    
+    if user_input == expected_parameter:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
