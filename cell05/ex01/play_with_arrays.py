99a2227c60a5627e339d4b839a90b91f72483cd0
pingningnoi <ping.ningnoi@gmail.com>
2025-11-06 09:23:01 +0000

    completed

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+new_array = []
+
+for number in original_array:
+    new_number = number + 2
+    new_array.append(new_number)
+
+print(f"Original array: {original_array}")
+print(f"New array: {new_array}")
